2dde303b232ca388b2e968775bbb0802392ead38
ADLPT-0291 <113531864+ADLPT-0291@users.noreply.github.com>
2024-03-22 15:24:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,7 @@
 # limitations under the License.
 #
 import time
-import paho.mqtt.client as paho
-from paho import mqtt
+import paho.mqtt.client as mqtt
 
 # setting callbacks for different events to see if it works, print the message etc.
 def on_connect(client, userdata, flags, rc, properties=None):
@@ -36,11 +35,16 @@ def on_message(client, userdata, msg):
 # using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31
 # userdata is user defined data of any type, updated by user_data_set()
 # client_id is the given name of the client
-client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
+# client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
+# client.on_connect = on_connect
+
+# # enable TLS for secure connection
+# client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+client = mqtt.Client()
 client.on_connect = on_connect
 
 # enable TLS for secure connection
-client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
 # set username and password
 client.username_pw_set("gtechdn", "gtechdn123")
 # connect to HiveMQ Cloud on port 8883 (default for MQTT)
